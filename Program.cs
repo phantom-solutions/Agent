8ed1e3489400894dab94b792215063d07a94fbac
Nicole Rappe <cyberstrawberry101@gmail.com>
2020-12-20 12:16:48 -0700

    Basic Deployment
    CLI-based C# application acting as the Borealis CP Agent deployed to GitHub.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Borealis_Server_Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ControlPanel_IPAddress = null;
+            Console.WriteLine("Initializing First-Time Launch of Borealis Server Agent...");
+            Console.WriteLine("Please Enter IP Address of Control Panel:");
+            ControlPanel_IPAddress = Console.ReadLine();
+
+            Console.WriteLine("You entered:{0}, press ENTER to confirm.", ControlPanel_IPAddress);
+            Console.ReadLine(); //Pause
+        }
+    }
+}
