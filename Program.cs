a9033fc5c769b8247bb943e7ada503adba33af3b
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-09 21:12:29 -0700

    Converted project from .NET Core to .NET Framework

--- Program.cs
+++ Program.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
 
-namespace Borealis_Server_Agent
+namespace Borealis_Agent
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Borealis Server Agent - DataTransfer Pipeline Test: \n");
-            Console.ReadLine();
-        }   
+        }
     }
 }
