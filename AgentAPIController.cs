b88b541119e39e11c24b98a466dfa66291238a6c
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-09 22:43:04 -0700

    Added RESTful API to Agent

---
+++ AgentAPIController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Borealis_Agent
+{
+    public class AgentAPIController : ApiController
+    {
+        public PayloadClass payloadInstance = new PayloadClass();
+
+        public Agent GetAgent()
+        {
+            return new Agent
+            {
+                AgentGUID = "d6871a01-ca61-4754-9dcb-0db774fda886",
+                AgentIP = "000.000.0.0",
+                AgentHOSTNAME = "Dummy Hostname"
+            };
+        }
+
+        public void SetPayload(int id, [FromBody] string data)
+        {
+            if (id == 1337)
+            {
+                payloadInstance.PayloadData = data;
+            }
+        }
+
+        public string GetPayload()
+        {
+            return payloadInstance.PayloadData;
+        }
+
+        // GET: http://localhost:8080/AgentAPI/Get
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: http://localhost:8080/AgentAPI/Get/5
+        public string Get(int id)
+        {
+            int WebHook = id * 5;
+            return WebHook.ToString();
+        }
+
+        // POST: http://localhost:8080/AgentAPI/Get
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: http://localhost:8080/AgentAPI/Put/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Agent
+    {
+        public String AgentGUID { get; set; }
+        public String AgentIP { get; set; }
+        public String AgentHOSTNAME { get; set; }
+    }
+
+    public class PayloadClass
+    {
+        public String PayloadData { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http.Routing;
+using System.Web.Http;
 
 namespace Borealis_Agent
 {
@@ -10,6 +13,21 @@ namespace Borealis_Agent
     {
         static void Main(string[] args)
         {
+            Console.Write("Borealis Server Agent - Starting API Controller... \n");
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute(
+                name: "API",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            using (HttpSelfHostServer agentAPIserver = new HttpSelfHostServer(config))
+            {
+                agentAPIserver.OpenAsync().Wait();
+                Console.Write("Borealis Server Agent - API Controller Initialized and listening on http://localhost:8080");
+                Console.ReadLine();
+            }
         }
     }
 }
