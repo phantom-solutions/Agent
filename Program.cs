9f19523ed291afbea13ca49e6c8e55a6fc76e883
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-15 01:01:10 -0700

    Added SQLite database functionality.

--- Program.cs
+++ Program.cs
@@ -23,8 +23,18 @@ namespace Borealis_Agent
 
             using (HttpSelfHostServer agentAPIserver = new HttpSelfHostServer(config))
             {
+                //Startup Message
+                Console.WriteLine("Borealis Agent v0.01");
+
+                //Start the API Controller
+                Console.Write("{0} | Start API Controller on http://localhost:8080: ", DateTime.Now);
                 agentAPIserver.OpenAsync().Wait();
-                Console.WriteLine("{0} | Borealis Server Agent - API controller started and listening on http://localhost:8080", DateTime.Now);
+                Console.Write("Done\n");
+
+                //Initialize the SQLite Test Database and Check its Version
+                Console.Write("{0} | Start SQLite Test Database: ", DateTime.Now);
+                Console.Write("Done (Version {0})\n", SQLite_Database.CheckDBVersion());
+                
                 Console.Write("{0} | Please enter the IP address of the Borealis Control Panel (Leave blank to use localhost): ", DateTime.Now);
                 string BorealisServerIP = Console.ReadLine();
 
---
+++ SQLite_Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Borealis_Agent
+{
+    class SQLite_Database
+    {
+        public static string CheckDBVersion()
+        {
+            string cs = "Data Source=:memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            return version;
+        }
+    }
+}
