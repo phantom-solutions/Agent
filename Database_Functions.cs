45760f0e0caa6d3b74d4b408f1d416d513c5c162
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-18 21:26:29 -0700

    Further progress on storing data into Database.

--- Database_Functions.cs
+++ Database_Functions.cs
@@ -38,7 +38,7 @@ namespace Borealis_Agent
 
             Console.WriteLine("{0} | [DATABASE] Creating Borealis Control Panel Data Table", DateTime.Now);
             //Create Control Panel Table Data
-            string sql = "CREATE TABLE controlpanel_data (ip_address VARCHAR(15), guid VARCHAR(36))";
+            string sql = "CREATE TABLE borealis_data (ip_address VARCHAR(15), guid VARCHAR(36))";
             SQLiteCommand generateCPTable = new SQLiteCommand(sql, m_dbConnection);
             generateCPTable.ExecuteNonQuery();
 
@@ -62,6 +62,22 @@ namespace Borealis_Agent
             string HOSTNAME = Dns.GetHostName();
             string sql = $"insert into agent_data (ip_address, guid) values ('{IP}', '{GUID}')";
             
+            //Insert the data into the agent table
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            m_dbConnection.Close();
+        }
+
+        public static void StoreCPData(string HOSTNAME, string IP, string GUID)
+        {
+            //Connect to the database
+            SQLiteConnection m_dbConnection = new SQLiteConnection($"Data Source={DB_File};Version=3;");
+            m_dbConnection.Open();
+
+            //Pull the local agent data to be populated into the agent database table
+            string sql = $"insert into borealis_data (hostname, ip_address, guid) values ('{HOSTNAME}', '{IP}', '{GUID}')";
+
             //Insert the data into the agent table
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             command = new SQLiteCommand(sql, m_dbConnection);
